6f0c560702e7220ba15cf34f6ca10b7bc902e583
Nikolay Yozov <nikolayyozov@Nikolays-MacBook-Pro.local>
2021-11-08 12:53:09 -0500

    fix bug in transition and back funcs

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -29,7 +29,7 @@ export default function Appointment(props) {
       interviewer,
     };
 
-    transition(SAVING, true);
+    transition(SAVING);
 
     props
       .bookInterview(props.id, interview)
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,26 +1,25 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  // const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = function (newMode, replace = false) {
-    setMode(newMode);
+  const transition = function (mode, replace = false) {
     if (replace) {
-      const newHistory = history;
-      newHistory.pop();
-      newHistory.push(newMode);
-      setHistory(newHistory);
+     
+      setHistory(prev => [...prev.slice(0, prev.length - 1), mode])
+
+
     } else {
-      setHistory((prev) => [...prev, newMode]);
+      setHistory((prev) => [...prev, mode]);
     }
   };
   const back = function () {
-    if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
+    if (history.length < 2) {
+      return
     }
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
   };
 
-  return { mode, transition, back };
+  return { mode: history[history.length - 1], transition, back };
 }
